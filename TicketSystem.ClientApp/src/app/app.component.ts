ef696790527a1fb0854339aa64eac5b900e43b1c
ftmbtlgr <ftmbtlgr@gmail.com>
2025-07-07 16:33:26 +0300

    Api settings

--- TicketSystem.ClientApp/src/app/app.component.ts
+++ TicketSystem.ClientApp/src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./layout/header/header.component";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  protected title = 'hello';
+export class AppComponent implements OnInit {
+  baseUrl = 'https://localhost:5001/api/';
+  private http = inject(HttpClient);
+  title = 'hello';
+
+  ngOnInit(): void {
+    this.http.get(this.baseUrl + 'users').subscribe({
+      next: data => console.log(data),
+      error: error => console.log(error),
+      complete: () => console.log('complete')
+  })
+  }
 }
